6a12bd86ac2bc8ae9de2b44fc2c857374e4976df
Taylor <nine1001@126.com>
2022-01-23 17:59:32 +0800

    add demo project

---
+++ src/Nuway/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuway
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceCollectionExtension
+    {
+
+    }
+}
---
+++ src/Nuway/ServiceConfigureExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuway
+{
+    /// <summary>
+    /// <para>service configure extension</para>
+    /// </summary>
+    /// <remarks> using this class static methods in startup class configure method. </remarks>
+    public static class ServiceConfigureExtension
+    {
+        /// <summary>
+        /// Use Nuway in your application
+        /// </summary>
+        /// <param name="app">current application object</param>
+        /// <returns>IApplicationBuilder</returns>
+        public static IApplicationBuilder UseNuway(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
